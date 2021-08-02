2d330d3970dc33305f6fa182c3cff779059dc75e
guchi3f4 <guchi3f4@gamil.com>
2021-08-02 10:35:33 +0000

    4-10

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :books, dependent: :destroy
   attachment :profile_image
-  validates:name,presence:{ message: "is too short (minimum is 2 characters)" }
+  validates:name,
+    uniqueness: true,
+    length: { in: 2..20 }
+  validates:introduction,
+    length: { maximum: 50 }
 end
