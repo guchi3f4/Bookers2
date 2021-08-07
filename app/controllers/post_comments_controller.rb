b55bb0d2f3d22a1a0b9b4549ecfd319de3501485
guchi3f4 <guchi3f4@gmail.com>
2021-08-07 07:40:56 +0000

    [Finish]5-3

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,10 +1,15 @@
 class PostCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    post_comment = current_user.post_comments.new(book_params)
-    post_comment.book_id = book.id
-    post_comment.save
-    redirect_to book_path(params[:book_id])
+    @user_book = Book.find(params[:book_id])
+    @post_comment = current_user.post_comments.new(book_params)
+    @post_comment.book_id = @user_book.id
+    if @post_comment.save
+      redirect_to book_path(params[:book_id])
+    else
+      @user = Book.find(params[:book_id]).user
+      @book = Book.new
+      render "books/show"
+    end
   end
 
   def destroy
